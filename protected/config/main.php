2f91ba1c05eca86b97fd1a11e0c326c28c6e1220
Alexander Ponomarenko <ap@nikolasoft.ru>
2016-03-25 22:56:23 +0400

    protected/data/testdrive.db -> protected/data/testdrive.sqlite
    ver 0.1

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'НиколаСофт',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -48,7 +48,7 @@ return array(
 		),
 		*/
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.sqlite',
 		),
 		// uncomment the following to use a MySQL database
 		/*
